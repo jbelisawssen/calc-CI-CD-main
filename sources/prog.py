deba8a2af6ad409209d97b107d57f2dadd8cdc98
unknown <jbelisawssen91@gmail.com>
2023-11-18 21:28:31 +0100

    first commit

--- sources/prog.py
+++ sources/prog.py
@@ -3,25 +3,6 @@ import calc
 import sys
 
 #first commit1 
-print("Bonjour Jb Saoussen.\n")
+print("Bonjour Jvb Saoussen.\n")
 
 print("Bienvenue dans cette petite calculatrice sous Python pour entier.\n")
-
-
-def run_calc():
-    op = input("Choisissez une opération entre +, -, x , / et %      : \n")
-    term_1 = input("Entrer votre premier entier \n")
-    term_2 = input("Enter votre second entier \n")
-    res = calc.ope(op, term_1, term_2)
-    if res != None:
-        print("{} {} {} = {}".format(term_1, op, term_2, res))
-        keep_going = input(
-            "Voulez-vous stopper le programme ? Écrivez Oui, dans ce cas\n"
-        )
-        if keep_going == "Oui":
-            sys.exit(0)
-    print("Nous allons recommencer le programme\n")
-    run_calc()
-
-
-run_calc()
