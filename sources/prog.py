abd5d832ea1f0ca9c3078f4794f6ba01fd9e2d7c
unknown <jbelisawssen91@gmail.com>
2023-12-02 20:19:44 +0100

    presentation

--- sources/prog.py
+++ sources/prog.py
@@ -4,6 +4,6 @@ import sys
 #commit3
 
 #first commit1 
-print("Bonjour Jvb Saoussen.\n")
+print("Bonjour Jbali Saoussen.\n")
 
 print("Bienvenue dans cette petite calculatrice sous Python pour entier.\n")
